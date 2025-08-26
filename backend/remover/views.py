47e6152a9c303b8dc16a3c8ad9491d581f821f1f
Matheus Barbosa <matheusalolbarbosa@gmail.com>
2025-08-26 14:00:41 -0300

    feat: expose remove-background endpoint (stub)

--- backend/remover/views.py
+++ backend/remover/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class RemoveBackgroundView(APIView):
+    def post(self, request, format=None):
+        return Response({"message": "Endpoint is alive"}, status=status.HTTP_200_OK)
